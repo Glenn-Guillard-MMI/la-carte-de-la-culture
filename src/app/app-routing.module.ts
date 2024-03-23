bfb8bc0bc296c0decdf40f64108115749cebb8cf
Jeremie-PARANT <parantjeremie@gmail.com>
2024-03-23 17:25:09 +0100

    importation page Japon + front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarteComponent } from './composants/carte/carte.component';
 import { GlennJeromeMorganeComponent } from './composants/glenn-jerome-morgane/glenn-jerome-morgane.component';
+import { JaponComponent } from './composants/japon/japon.component';
 
 const routes: Routes = [
   { path: '', component: CarteComponent },
   { path: 'Egypte', component: GlennJeromeMorganeComponent },
+  { path: 'Japon', component: JaponComponent }
 ];
 
 @NgModule({
